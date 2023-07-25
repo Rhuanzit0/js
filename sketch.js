8c9b77551276c4674bb76f1f759928cda7474100
Rhuanzit0 <140512767+Rhuanzit0@users.noreply.github.com>
2023-07-25 09:56:17 -0300

    uploud arquivos

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+    createCanvas(400, 400);
+     background(1);
+  }
+  
+  function draw() {
+   
+  stroke("blue") 
+   //console.log(mouseIsPressed);
+    if(mouseIsPressed){
+      rect(mouseX, mouseY, 15, 15);  
+    }
+    
+    
+    
+    
+  fill("rgb(255,0,0)")  
+  
+  }
+ 
